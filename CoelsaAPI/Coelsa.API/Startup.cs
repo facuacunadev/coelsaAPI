c6885920b3b165bd83f7ac90b345c3bc7523b5a8
facundo acuña <32574782+facuacunia@users.noreply.github.com>
2021-02-01 12:09:28 -0300

    add swagger library

--- CoelsaAPI/Coelsa.API/Startup.cs
+++ CoelsaAPI/Coelsa.API/Startup.cs
@@ -1,19 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Coelsa.Business.ContactManager.Implementation;
 using Coelsa.Business.ContactManager.Interface;
 using Coelsa.Data.Repository.Implementation;
 using Coelsa.Data.Repository.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Coelsa.API
 {

28f88073da1f1d18bd0444664c394048a65937e2
facundo acuña <32574782+facuacunia@users.noreply.github.com>
2021-02-02 21:06:47 -0300

    add swagger to the api

--- CoelsaAPI/Coelsa.API/Startup.cs
+++ CoelsaAPI/Coelsa.API/Startup.cs
@@ -27,6 +27,9 @@ namespace Coelsa.API
             services.AddScoped<IContactManager, ContactManager>();
             services.AddScoped<IRepository, ContactRepository>();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
@@ -39,6 +42,17 @@ namespace Coelsa.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Contacts API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
